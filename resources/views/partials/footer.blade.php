5353bceed1584cfee613e60b8f9ce321c4317755
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-06 20:13:13 +0100

    Implemented macrostructure of header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,48 @@
+<footer>
+    <section id="upper-footer">
+        <div class="container">
+            <article class="ftr-list-box">
+                <ul class="ftr-list">
+                    <li class="list-title"><h3>Lorem</h3></li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+                <ul class="ftr-list">
+                    <li class="list-title"><h3>Lorem</h3></li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+            </article>
+            <article class="ftr-list-box">
+                <ul class="ftr-list">
+                    <li class="list-title"><h3>Lorem</h3></li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+            </article>
+            <article class="ftr-list-box">
+                <ul class="ftr-list">
+                    <li class="list-title"><h3>Lorem</h3></li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+            </article>
+        </div>
+    </section>
+    <section id="bottom-footer">
+        <div class="container">
+           <h1>BottomFooter</h1> 
+        </div>
+    </section>
+</footer>
